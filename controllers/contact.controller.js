94d76376743237a26bcd3693486eabb668a60509
Aslam Parvej <aslamparvej2@gmail.com>
2025-11-11 16:12:03 +0530

    Phase 2: Changed Contact Form

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -15,9 +15,8 @@ const transporter = nodemailer.createTransport({
 });
 
 
-
 function postContact(req, res) {
-    const { name, email, phone, message } = req.body;
+    const { name, email, subject, message } = req.body;
 
     try {
 
@@ -25,11 +24,10 @@ function postContact(req, res) {
             from: email,
             to: process.env.EMAIL_USER,
             subject: `New Contact Form Submission from ${name}`,
-            // text: `You have a new message from ${name}(${email}):\n\n${message}`,
             html: `<div class="mail-template-container" style="height: 100%; width: 100%; background: #EAF0F3; font-family: Arial; color: #5E5E5E; font-size: 16px;font-weight: 400;line-height: 26px;">
             <div class="mail-template" style="width: 40rem; margin: 0 auto; height: 100%; padding: 1rem;">
             <div class="client-logo-container" style="margin-bottom: 2rem;">
-            <img src="https://devcenter.in/img/assets/website-logo.png" alt="enerv global logo" />
+            <img src="https://devcenter.in/img/assets/favicon.png" alt="dev center logo" />
             </div>
             <div class="mail-content" style="background: #FFFFFF; padding: 2rem;">
                 <div class="mail-content-header" style="display: flex; align-items: center; gap: 1rem; margin-bottom: 1rem;">
@@ -55,16 +53,19 @@ function postContact(req, res) {
 
         transporter.sendMail(mainOptions, (error, info) => {
             if (error) {
+                return res.status(500).json({ success: false, message: "Failed to send email" });
                 // return res.status(500).send('There was an error processing your request. Please try again later.');
-                return res.render('user/includes/alert', { title: "Message Send", message: "There was an error processing your request. Please try again later.", icon: "error", confirmButtonText: "Ok", redirectLocation: "/contact" });
+                //return res.render('user/includes/alert', { title: "Message Send", message: "There was an error processing your request. Please try again later.", icon: "error", confirmButtonText: "Ok", redirectLocation: "/contact" });
             } else {
+                return res.status(200).json({ success: true, message: "Email sent successfully" });
                 // return res.send('Thank you for contacting us! We will get back to you shortly.');
-                return res.render('user/includes/alert', { title: "Message Send", message: "Thank you for contacting us! We will get back to you shortly.", icon: "success", confirmButtonText: "Ok", redirectLocation: "/contact" });
+                // return res.render('user/includes/alert', { title: "Message Send", message: "Thank you for contacting us! We will get back to you shortly.", icon: "success", confirmButtonText: "Ok", redirectLocation: "/contact" });
             }
         });
 
     } catch (error) {
         res.json({ Message: error });
+        return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
